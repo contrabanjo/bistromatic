7f00ecb5c2aa1c409b7e678d6a1cf6ea323cef8b
Corrie Grimshaw <corrie@pcg.local>
2021-06-09 16:05:44 -0700

    Refactor: Update logic for appearance of center content

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function initMap(){
 }
 
 function getRestaurant(){
+  document.getElementById('center').classList.remove("grow");
+  document.getElementById('center-text').classList.remove("grow");
   //var home = new google.maps.LatLng(32.910456236918485, -117.16073383095717);
   var miramesa = new google.maps.LatLng(32.91790652586681, -117.11554620476238);
 
@@ -17,13 +19,13 @@ function getRestaurant(){
 
   service.nearbySearch(request, function(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-       console.log(results);
        const rand = Math.floor(Math.random() * results.length);
        const choice = results[rand];
        
        document.getElementById('name').innerHTML = choice.name;
        document.getElementById('address').innerHTML = choice.vicinity;
-       document.getElementById('center-text').style.animation ='grow 2.5s';
+       document.getElementById('center-text').classList.add("grow");
+       document.getElementById('center').classList.add("grow");
 
        document.getElementById('center').style.backgroundImage = 'url("' + choice.photos[0].getUrl() + '")';
        document.getElementById('center').style.animation = 'grow 2s';
